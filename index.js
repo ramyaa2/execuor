89365aaa43951f8730eb266d688d450cfa56a2e9
ramyaa2 <125340959+ramyaa2@users.noreply.github.com>
2024-03-09 00:04:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const axios = require("axios");
 
 const app = express();
-const port = 3000;
+const port = 10000;
 
 app.use(express.json());
 app.get("/", async (req, res) => {
